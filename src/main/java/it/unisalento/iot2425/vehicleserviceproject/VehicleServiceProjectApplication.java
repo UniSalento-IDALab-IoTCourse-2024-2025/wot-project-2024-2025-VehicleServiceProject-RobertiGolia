1480c0e63445fdb7551d18f0e9780a3813c00b09
Luigi Roberti <luigi.roberti@studenti.unisalento.it>
2025-06-04 22:54:06 +0200

    Creazione progetto

---
+++ src/main/java/it/unisalento/iot2425/vehicleserviceproject/VehicleServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package it.unisalento.iot2425.vehicleserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleServiceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unisalento/iot2425/vehicleserviceproject/VehicleServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisalento.iot2425.vehicleserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
